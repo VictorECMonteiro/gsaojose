47b351d09756d98509929e1bb68191f4ff45a0fe
VictorEmanuel <victoremanuel14769@gmail.com>
2025-12-10 11:21:26 -0300

    Adicionado seção produtos

--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import Footer from '../../components/Footer/Footer'
 import ZezinhoBandejaDeOvos from "../../assets/Zezinho_bandeja_de_ovos.png"
@@ -7,14 +7,41 @@ import NutriEggs from "../../assets/selo_nutrieggs.png"
 import "./Produtos.css"
 import { ScrollEntering } from './ProdutoAnimations'
 import { Helmet } from 'react-helmet-async'
+import { StrapiGet } from '../../configuration/strapiApi'
+const url = `${import.meta.env.VITE_StrapiAdress}`;
+interface Produtos {
+    NomeProduto: string,
+    FotoProduto: {
+        url: string
+    }
+    DescricaoFotoProduto: string,
+    DescricaoProdutoPrincipal?: string,
+    DescricaoProdutoSecundaria?: string,
+    TabelaNutricional: {
+        url: string
+    }
+    TabelaNutricionalTexto: string
+
+}
+
 
 export default function Produtos() {
+    const [Produtos, setProdutos] = useState<[Produtos]>()
     ScrollEntering(".zezinhoSaoJose")
+    useEffect(() => {
+        StrapiGet("sao-jose-produtos").then((res: any) => {
+            setProdutos(res.data)
+            console.log(res.data)
+            console.log(res.data)
+        })
+
+    }, [])
+
     return (
         <div className='container'>
             <Helmet>
                 <title>Produtos - Granja Sao Jose</title>
-                <meta name='description' content='Nossos produtos, fabricados com alto rigor de qualidade, e uma imensa dose de carinho'/>
+                <meta name='description' content='Nossos produtos, fabricados com alto rigor de qualidade, e uma imensa dose de carinho' />
             </Helmet>
             <NavBar />
             <section className='apresentacao'>
@@ -27,12 +54,12 @@ export default function Produtos() {
                     </h2>
                 </div>
                 <div className='apresentacao-banner'>
-                    <img src={ZezinhoBandejaDeOvos} alt="Zezinho ao lado de bandeija de ovos São José" className='zezinhoSaoJose'/>
+                    <img src={ZezinhoBandejaDeOvos} alt="Zezinho ao lado de bandeija de ovos São José" className='zezinhoSaoJose' />
                 </div>
                 <div className='apresentacao-nutrieggs'>
                     <div>
                         <img src={NutriEggs} alt="" />
-                        
+
                         <p>
                             O selo <i>NUTRIEGGS</i> une nutrição de qualidade e ovos produzidos com cuidado, pensado para quem valoriza saúde e bem-estar.
                             É mais do que um nome, é um símbolo de confiança na sua escolha por uma vida mais saudável, <b>com você todos os dias</b>
@@ -41,6 +68,30 @@ export default function Produtos() {
                     </div>
                     <img src={ZezinhoCestaDeOvos} alt="" />
                 </div>
+
+            </section>
+            <section className='Produtos'>
+                
+                    <h1>Linha de nutrição completa</h1>
+                    <h2>Tudo o que você precisa para levar mais sabor e saúde à sua mesa.</h2>
+                
+                <div className='ProdutosWrapper'>
+                {
+                    Produtos?.map((item, index) => (
+                        <div key={index} className='ProdutoContainer'>
+                            <img src={`${url}${item.FotoProduto.url}`} alt="" />
+                            <button>Tabela Nutricional</button>
+                            <h1>{item.NomeProduto}</h1>
+                            {item.DescricaoProdutoPrincipal ? item.DescricaoProdutoPrincipal : ""}
+                        </div>
+                    ))
+                }
+                </div>
+
+
+
+
+
 
             </section>
 
