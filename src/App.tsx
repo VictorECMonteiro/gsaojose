5cbd3d73b29c593a49b6c8a7e0f61a06bcf0c4f4
ketely17 <ketely.sena@appstijuca.com.br>
2025-12-30 09:05:42 -0300

    WIWIWIWI

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { BrowserRouter, Routes, Route} from "react-router";
-import './App.css'
-import Landing from './pages/Landing/Landing';
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./App.css";
+import Landing from "./pages/Landing/Landing";
 import Quemsomos from "./pages/Quemsomos/Quemsomos";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Produtos from "./pages/Produtos/Produtos";
-import Receitas from "./pages/Receitas/Receitas"
+import Receita from "./pages/Receita/Receita";
 
 function App() {
- 
   return (
     <>
       <div>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/quemsomos" element={<Quemsomos />}/>
+            <Route path="/quemsomos" element={<Quemsomos />} />
             <Route path="/produtos" element={<Produtos />} />
-            <Route path="/receitas/:slug" element={<Receitas />} />
+            <Route path="/receitas/:slug" element={<Receita />} />
           </Routes>
         </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
